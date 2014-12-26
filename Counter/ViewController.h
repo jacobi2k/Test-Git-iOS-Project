4510e36801e0f5a97f8a5f839cc727b9278e7926
jacoblanger <jacoblanger@Jacobs-MacBook-Pro.local>
2014-12-26 21:01:36 +0100

    Initial Commit

---
+++ Counter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Counter
+//
+//  Created by jacoblanger on 2014-12-26.
+//  Copyright (c) 2014 Jacob Langer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Counter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Counter
+//
+//  Created by jacoblanger on 2014-12-26.
+//  Copyright (c) 2014 Jacob Langer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
