289263493a9cb8fd4fa6fccd0fd5ddbed90f3492
jacoblanger <jacoblanger@Jacobs-MacBook-Pro.local>
2014-12-31 02:43:18 +0100

    first commit

--- Counter/ViewController.m
+++ Counter/ViewController.m
@@ -17,6 +17,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int answerForFactorial = [self factorialOfInteger:3];
+    NSLog(@"%i", answerForFactorial);
+}
+
+-(void)printAllWholeNumbersBelow:(int)number
+{
+    if (number > 0)
+    {
+        for (int i = number; i > 0; i--)
+        {
+            NSLog(@"%i",i);
+        }
+    }
+}
+
+-(void)printWholeNumbersInBetweenFirstInteger:(int)firstInteger andsecondInteger:(int)secondInteger
+{
+    if (firstInteger == secondInteger)
+    {
+        NSLog(@"The numbers are equal!");
+    }
+    else if (firstInteger > secondInteger)
+    {
+        for (int i = firstInteger; i <= secondInteger; i --)
+        {
+            NSLog(@"%i",i);
+        }
+    }
+    else
+    {
+        for (int i = secondInteger; i >= firstInteger; i--)
+        {
+            NSLog(@"%i", i);
+        }
+    }
+    
+}
+
+-(int)factorialOfInteger:(int)number
+{
+    int answer = number;
+    for (int i = number; i > 1; i --)
+    {
+        answer = answer * (i-1);
+    }
+    return answer;
 }
 
 - (void)didReceiveMemoryWarning {
